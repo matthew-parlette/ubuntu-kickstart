3758e858a75bd24ca8153918f6f1344abf0300ea
Matt Parlette <matthew.parlette@gmail.com>
2013-07-03 08:55:24 -0400

    Added fonts zip and conf.
    Closes #3

--- kickstart.sh
+++ kickstart.sh
@@ -5,6 +5,8 @@
 # Configuration #
 #################
 
+install_fonts=true
+
 # Development Libraries
 dev="build-essential"
 
@@ -13,3 +15,23 @@ ide="geany"
 
 # Utilities
 util="keepassx"
+
+# Install and configure fonts
+#############################
+if $install_fonts ; then
+  sudo unzip fonts.zip -d /usr/share/fonts
+  ## Ubuntu doesn't have .fonts.conf in the user dir by default,
+  ##  so we can just do a straight copy
+  cp fonts.conf ~/.fonts.conf
+fi
+
+# User post-install actions
+###########################
+if $install_fonts ; then
+  echo "To configure fonts on chrome, set the following:"
+  echo "\tStandard Font - Sans"
+  echo "\tSerif Font - Serif"
+  echo "\tSans-serif font - Sans"
+  echo "\tFixed-width font - monospace"
+  echo "\tMinimum font-size - 11px"
+fi
