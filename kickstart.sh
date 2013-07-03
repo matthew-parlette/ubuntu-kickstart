360a5be90a903a83802efea0288db5be989a5043
Matt Parlette <matthew.parlette@gmail.com>
2013-07-03 09:05:18 -0400

    Added status messages and fixed end text formatting.

--- kickstart.sh
+++ kickstart.sh
@@ -19,19 +19,22 @@ util="keepassx"
 # Install and configure fonts
 #############################
 if $install_fonts ; then
-  sudo unzip fonts.zip -d /usr/share/fonts
+  echo "Installing fonts..."
+  sudo unzip fonts.zip -d /usr/share/fonts > /dev/null
   ## Ubuntu doesn't have .fonts.conf in the user dir by default,
   ##  so we can just do a straight copy
   cp fonts.conf ~/.fonts.conf
+  echo "...ok"
 fi
 
 # User post-install actions
 ###########################
 if $install_fonts ; then
-  echo "To configure fonts on chrome, set the following:"
-  echo "\tStandard Font - Sans"
-  echo "\tSerif Font - Serif"
-  echo "\tSans-serif font - Sans"
-  echo "\tFixed-width font - monospace"
-  echo "\tMinimum font-size - 11px"
+  echo -e "\n\nTo configure fonts on chrome, set the following:"
+  echo "------------------------------------------------"
+  echo -e "\tStandard Font - Sans"
+  echo -e "\tSerif Font - Serif"
+  echo -e "\tSans-serif font - Sans"
+  echo -e "\tFixed-width font - monospace"
+  echo -e "\tMinimum font-size - 11px"
 fi
